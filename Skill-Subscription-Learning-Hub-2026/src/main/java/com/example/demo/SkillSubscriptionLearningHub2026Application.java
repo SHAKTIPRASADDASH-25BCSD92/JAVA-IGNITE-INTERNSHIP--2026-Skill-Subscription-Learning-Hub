3863d9db7ab0b00b44cf31931854be9ef2ba10de
SHAKTIPRASADDASH-25BCSD92 <dashshakti07@gmail.com>
2026-06-19 00:48:16 +0530

    Update SkillSubscriptionLearningHub2026Application.java

--- Skill-Subscription-Learning-Hub-2026/src/main/java/com/example/demo/SkillSubscriptionLearningHub2026Application.java
+++ Skill-Subscription-Learning-Hub-2026/src/main/java/com/example/demo/SkillSubscriptionLearningHub2026Application.java
@@ -2,12 +2,17 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.skills.hub")
+@EnableJpaRepositories(basePackages = "com.skills.hub.repository")
+@EntityScan(basePackages = "com.skills.hub.model")
 public class SkillSubscriptionLearningHub2026Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SkillSubscriptionLearningHub2026Application.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(SkillSubscriptionLearningHub2026Application.class, args);
+    }
 }
